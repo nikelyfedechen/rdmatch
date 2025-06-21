f4b1adfea023b926a540e209033879c0b0f87ee3
Nikely Fedechen <fedechenikely@gmail.com>
2025-06-21 16:41:30 -0300

    refactor(RecommendationType): remove duplicidade de código e adiciona htmlFor corretamente

--- frontend/src/components/Form/Fields/RecommendationType.js
+++ frontend/src/components/Form/Fields/RecommendationType.js
@@ -2,26 +2,34 @@ import React from 'react';
 import Checkbox from '../../shared/Checkbox';
 
 function RecommendationType({ onRecommendationTypeChange }) {
+  const recommendationTypes = [
+    {
+      id: 'SingleProduct',
+      label: 'Produto Único',
+    },
+    {
+      id: 'MultipleProducts',
+      label: 'Múltiplos Produtos',
+    },
+  ];
+
   return (
     <div className="mb-4">
       <h2 className="text-lg font-bold mb-2">Tipo de Recomendação:</h2>
-      <div className="flex items-center">
-        <Checkbox
-          type="radio"
-          name="recommendationType"
-          value="SingleProduct"
-          onChange={() => onRecommendationTypeChange('SingleProduct')}
-          className="mr-2"
-        />
-        <label htmlFor="SingleProduct" className="mr-4">Produto Único</label>
-        <Checkbox
-          type="radio"
-          name="recommendationType"
-          value="MultipleProducts"
-          onChange={() => onRecommendationTypeChange('MultipleProducts')}
-          className="mr-2"
-        />
-        <label htmlFor="MultipleProducts">Múltiplos Produtos</label>
+      <div className="flex items-center gap-8">
+        {recommendationTypes.map(({ id, label }) => (
+          <label className="flex items-center" htmlFor={id} key={id}>
+            <Checkbox
+              type="radio"
+              name="recommendationType"
+              className="mr-0"
+              id={id}
+              value={id}
+              onChange={() => onRecommendationTypeChange(id)}
+            />
+            {label}
+          </label>
+        ))}
       </div>
     </div>
   );
