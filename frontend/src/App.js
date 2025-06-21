ad16e478d56323b07a8090b838dc59ada7897886
Nikely Fedechen <fedechenikely@gmail.com>
2025-06-21 15:48:00 -0300

    refactor(layout): melhora responsividade, evitando sobreposição entre blocos e amplia área de conteúdo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,11 @@ function App() {
     useRecommendations(products);
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center py-16 px-8">
       <h1 className="text-3xl font-bold mb-8">
         Recomendador de Produtos RD Station
       </h1>
-      <div className="bg-white p-8 rounded-lg shadow-md w-full md:w-3/4 lg:w-1/2 grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full md:w-3/4 lg:w-2/3 grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="col-span-2 mb-4">
           <p className="text-lg">
             Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode
@@ -26,7 +26,7 @@ function App() {
             às suas necessidades.
           </p>
         </div>
-        <div>
+        <div className="col-span-2 md:col-span-1">
           <Form
             setRecommendations={setRecommendations}
             preferences={preferences}
@@ -34,7 +34,7 @@ function App() {
             getRecommendations={getRecommendations}
           />
         </div>
-        <div>
+        <div className="col-span-2 md:col-span-1">
           <RecommendationList recommendations={recommendations} />
         </div>
       </div>
