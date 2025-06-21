e419bcd4e65894de2f4e496b8d0dbb2864882d8a
Nikely Fedechen <fedechenikely@gmail.com>
2025-06-20 21:36:27 -0300

    feat(Form): adiciona lógica para atualizar recomendações

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,29 +1,24 @@
 // Form.js
-
-import React, { useEffect } from 'react';
 import { Preferences, Features, RecommendationType } from './Fields';
 import { SubmitButton } from './SubmitButton';
-import useProducts from '../../hooks/useProducts';
 import useForm from '../../hooks/useForm';
-import useRecommendations from '../../hooks/useRecommendations';
 
-function Form() {
-  const { preferences, features, products } = useProducts();
+function Form({
+  preferences,
+  features,
+  getRecommendations,
+  setRecommendations,
+}) {
   const { formData, handleChange } = useForm({
     selectedPreferences: [],
     selectedFeatures: [],
     selectedRecommendationType: '',
   });
 
-  const { getRecommendations, recommendations } = useRecommendations(products);
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const dataRecommendations = getRecommendations(formData);
-
-    /**
-     * Defina aqui a lógica para atualizar as recomendações e passar para a lista de recomendações
-     */
+    setRecommendations(dataRecommendations);
   };
 
   return (
